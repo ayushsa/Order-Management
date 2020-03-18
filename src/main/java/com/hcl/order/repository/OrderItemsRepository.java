3a4bbf1b1dda108285f88d0ca53a1c0ebc0e44eb
Ajeet Yadav <AjeetY@hcl.com>
2020-03-18 16:58:24 +0530

    initial commit

--- src/main/java/com/hcl/order/repository/OrderItemsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hcl.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.hcl.order.entity.OrderItem;
-
-public interface OrderItemsRepository extends JpaRepository<OrderItem, Long> {
-
-}
